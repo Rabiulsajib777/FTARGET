ab8b3d0dfce4cfef51a1a53c54b79b9378d6728d
Rabiul Sajib <99185124+Rabiulsajib777@users.noreply.github.com>
2022-02-13 11:06:04 +0600

    Add files via upload

---
+++ hunter.s
@@ -0,0 +1,17 @@
+import pyshorteners
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+print( Fore.GREEN + 'Hnter World ')
+
+
+print("""            │
+│                   ███████╗ █████╗      ██╗██╗██████╗                    │
+│                   ██╔════╝██╔══██╗     ██║██║██╔══██╗                   │
+│                   ███████╗███████║     ██║██║██████╔╝                   │
+│                   ╚════██║██╔══██║██   ██║██║██╔══██╗                   │
+│                   ███████║██║  ██║╚█████╔╝██║██████╔╝                   │
+│                   ╚══════╝╚═╝  ╚═╝ ╚════╝ ╚═╝╚═════""")
+print("                                 Creat By Rabiul Sajib                                       ")
+print('\033[39m ')
